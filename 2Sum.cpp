dcbd5eb296234659c25a7d482f8f907c946dfe76
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2024-12-21 22:14:45 +0530

    Update 2Sum.cpp

--- 2Sum.cpp
+++ 2Sum.cpp
@@ -18,3 +18,33 @@ vec.push_back(j);
         return vec;
     }
 };
+
+
+
+// better approach
+// TC: O(nlogn)
+dont know
+shraddha ne shayad wrong sikhaya haii
+
+
+
+// optimal approach
+// TC : O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        vector <int> vec;
+        unordered_map <int , int> m;
+        for(int i=0;i<n;i++) {
+            int first = arr[i];
+            int sec = target - first;
+            if(m.find(sec) != m.end()) {
+                vec.push_back(i);
+                vec.push_back(m[sec]);
+            }
+            m[first]=i;
+        }
+        return vec;
+    }
+};
